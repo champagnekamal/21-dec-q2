5162e2a9637986b09be4767b6d82af8a36b10007
champagnekamal <akashkamal70@gmail.com>
2021-12-29 19:39:53 +0530

    first

---
+++ q2.js
@@ -0,0 +1,17 @@
+var char;
+
+char = prompt("enter a character");
+
+if(char == "a") {
+    alert("It is a vowel");
+   } else if(char == "e") {
+    alert("It is a vowel");
+   } else if(char == "i") {
+    alert("It is a vowel");
+   } else if(char == "o") {
+    alert("It is a vowel");
+   } else if(char == "u") {
+    alert("It is a vowel");
+   } else {
+    alert("It is a consonant");
+   }
